7d2fdf74725ac4addfcae3b0dce3e1db97177637
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-03-13 21:00:28 -0300

    Actualizando layout

--- .configuracion/quartz.layout.ts
+++ .configuracion/quartz.layout.ts
@@ -3,48 +3,42 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-    },
-  }),
+    head: Component.Head(),
+    header: [],
+    footer: Component.Footer({
+        text: "Links",
+        links: {
+            GitHub: "https://github.com/JuanBiancuzzo/Facultad-Apuntes",
+        },
+    }),
+    left: [
+        Component.PageTitle(),
+
+        Component.MobileOnly(Component.Spacer()),
+        Component.MobileOnly(Component.Darkmode()),
+
+        Component.Search(),
+
+        Component.DesktopOnly(Component.Explorer()),
+
+    ],
+    beforeBody: [
+        Component.ArticleTitle(),
+        Component.Spacer(),
+        Component.Breadcrumbs(),
+    ],
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-    Component.TableOfContents(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.Spacer(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [
-    // Component.Graph(),
-    Component.Spacer(),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.Backlinks(),
-    // Component.DesktopOnly(Component.TableOfContents()),
-  ],
+    right: [
+        Component.DesktopOnly(Component.TableOfContents()),
+        Component.Backlinks(),
+    ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [Component.TableOfContents(),],
+    right: [],
 }
+
